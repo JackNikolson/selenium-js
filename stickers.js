9085a8bd964aa1921241a047f96eb2e8ba92e562
Irwin <chezareborja@gmail.com>
2019-08-19 21:20:16 +0300

    Add files via upload
    Проверка наличия стикеров у блоков товаров.

---
+++ stickers.js
@@ -0,0 +1,14 @@
+/* Сделайте сценарий, проверяющий наличие стикеров у всех товаров в учебном приложении litecart на главной странице. 
+Стикеры -- это полоски в левом верхнем углу изображения товара, на которых написано New или Sale или что-нибудь другое. 
+Сценарий должен проверять, что у каждого товара имеется ровно один стикер. */
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+(async () => {
+	let driver = new Builder().forBrowser('chrome').build();
+	driver.manage().setTimeouts( {implicit: 3000} );
+	await driver.get('http://localhost/litecart/en/');
+	let blocks = await driver.findElements(By.xpath('//a[@class="link" and @title]'));
+	await blocks.forEach(async function () {
+		await driver.wait(until.elementLocated(By.css('a.link div.sticker')), 2000);
+	});
+})(); 
